590621602b7bb0b660678a84b8c4211972f0da71
Airat-ASMPT-PC <airat.valiullin@asmpt.com>
2025-01-21 15:14:54 +0100

    partly add sparse matrices

--- src/representations.py
+++ src/representations.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.sparse as sp
 
 from src.common_types import Edge
 
@@ -6,5 +7,12 @@ from src.common_types import Edge
 def edge_list_to_adj_matrix(edges: list[Edge]) -> np.ndarray:
     """
     Convert a list of undirected edges to an adjacency matrix.
+    """
+    raise NotImplementedError
+
+
+def edge_list_to_adj_matrix_sparse(edges: list[Edge]) -> sp.csr_matrix:
+    """
+    Convert a list of undirected edges to a sparse adjacency matrix.
     """
     raise NotImplementedError
