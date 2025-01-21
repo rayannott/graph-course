f3e4be1edff7bd49843659cc1e6d4537e1416686
Airat-ASMPT-PC <airat.valiullin@asmpt.com>
2025-01-21 10:01:28 +0100

    refactor; add two problems

---
+++ src/representations.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from src.common_types import Edge
+
+
+def edge_list_to_adj_matrix(edges: list[Edge]) -> np.ndarray:
+    """
+    Convert a list of undirected edges to an adjacency matrix.
+    """
+    raise NotImplementedError
